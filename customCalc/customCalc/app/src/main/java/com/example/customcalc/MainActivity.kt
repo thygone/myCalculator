8aff3a17ffa2b77758b65b9fc85b61e55a40734d
thygone <75769635+thygone@users.noreply.github.com>
2021-12-01 23:01:48 -0800

    Update MainActivity.kt

--- customCalc/customCalc/app/src/main/java/com/example/customcalc/MainActivity.kt
+++ customCalc/customCalc/app/src/main/java/com/example/customcalc/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.example.customcalc
 
 
+//imports
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import android.text.Editable
 import android.widget.Button
 import android.widget.EditText
@@ -13,17 +13,21 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+    
+    //text boxes
+    lateinit var answer : TextView
     lateinit var number1 : EditText
     lateinit var number2 : EditText
+    
+    //buttons
     lateinit var add : Button
     lateinit var sub :Button
     lateinit var div : Button
     lateinit var mult : Button
     lateinit var clear : Button
-    lateinit var answer : TextView
+    
 
-
+    // this is will apply the id's to the respective objects above when the app is launched
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,7 +46,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
 
 
-
+// these functions will acttivate when the respective button is pushed
+// they will add, divide, subtract, or multiply the two inputed numbers
     fun addNumbs (a: Int, b: Int): Int {
         var c = a + b
         return c
@@ -52,15 +57,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         var c = a / b
         return c
     }
+    
     fun subt (a: Int, b: Int): Int {
         var c = a - b
         return c
     }
+    
     fun multt (a: Int, b: Int): Int {
         var c = a * b
         return c
     }
 
+    
+    // when a button is clicked this will grab the two values and then decide what function to use
+    // based on what the id of the pushed button is.
     override fun onClick(view: View?) {
         Toast.makeText(this, "Button Clicked", Toast.LENGTH_SHORT).show()
         val numbb1 = number1.text.toString()
@@ -85,12 +95,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                 ans = multt(numb1, numb2)
                 answer.setText(ans.toString())
             }
-
         }
-
     }
-
-
 }
 
 
